8a6b07baaa3c5b801e54a270a606623936f8ff01
IamFastre <mahmoudmode057@gmail.com>
2024-03-31 06:58:20 +0200

    edit to headers

--- components/applets/Header.tsx
+++ components/applets/Header.tsx
@@ -1,21 +1,46 @@
 import { TouchableOpacity, StyleSheet } from 'react-native';
-import { router } from 'expo-router';
+import { router, useNavigation } from 'expo-router';
 import { T, Section, C, B } from '@/components/basics';
 import { Ionicons } from '@expo/vector-icons';
 import { useColors } from '@/constants/colors';
 
-export function Header({title}:{title:string}) : React.JSX.Element {
+export function Header({title, back, options}:{title:string, back?:string, options:string}) : React.JSX.Element {
   const colors = useColors();
 
   return (
     <Section style={styles.header} containerStyle={styles.headerContainer}>
-      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+      <TouchableOpacity style={styles.backButton} onPress={() => !router.canGoBack() ? router.back() : router.dismiss()}>
         <Ionicons name='chevron-back-sharp' size={35} color={colors.hot} />
       </TouchableOpacity>
       <T style={styles.titleText}>
         <C.ACCENT>•-{'{ '}</C.ACCENT>
         <B>{title}</B>
         <C.ACCENT>{' }'}-•</C.ACCENT>
+      </T>
+      <TouchableOpacity style={styles.settingsButton} onPress={() => router.push(options as any)}>
+        <Ionicons name='options' size={30} color={colors.accent} />
+      </TouchableOpacity>
+    </Section>
+  );
+}
+
+export function MiniHeader({title, subtitle}:{title:string, subtitle:string}) : React.JSX.Element {
+  const colors = useColors();
+
+  return (
+    <Section style={styles.header} containerStyle={styles.headerContainer}>
+      <TouchableOpacity style={styles.backButton} onPress={() => !router.canGoBack() ? router.back() : router.dismiss()}>
+        <Ionicons name='close' size={35} color={colors.hot} />
+      </TouchableOpacity>
+      <T style={styles.titleText}>
+        <C.ACCENT>[</C.ACCENT>
+        <B> {title} </B>
+        <C.ACCENT>]</C.ACCENT>
+      </T>
+      <T>
+        <C.SECONDARY>
+          {subtitle}
+        </C.SECONDARY>
       </T>
     </Section>
   );
@@ -27,7 +52,6 @@ const styles = StyleSheet.create({
   },
 
   headerContainer: {
-    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -39,5 +63,10 @@ const styles = StyleSheet.create({
 
   titleText: {
     fontSize: 26,
-  }
+  },
+
+  settingsButton: {
+    position: "absolute",
+    right: 22.5
+  },
 });
