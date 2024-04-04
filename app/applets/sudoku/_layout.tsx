f60eb049b6496b3c5f12a9c7bf1bc46b0fdd37f7
IamFastre <mahmoudmode057@gmail.com>
2024-04-04 02:01:04 +0200

    more flexibility in navigation

--- app/applets/sudoku/_layout.tsx
+++ app/applets/sudoku/_layout.tsx
@@ -1,16 +1,3 @@
-import { Stack } from 'expo-router';
+import Layout from '@/components/applets/Layout';
 
-export default function Layout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        freezeOnBlur: true,
-        orientation: 'portrait',
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="settings" options={{ presentation: 'modal' }} />
-    </Stack>
-  );
-}
+export default Layout;
