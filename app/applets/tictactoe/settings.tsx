6ee9819540ef7a8a2a92dba81f942466f5d6d5a3
IamFastre <mahmoudmode057@gmail.com>
2024-04-12 05:59:48 +0200

    full settings for TTT

--- app/applets/tictactoe/settings.tsx
+++ app/applets/tictactoe/settings.tsx
@@ -1,15 +1,17 @@
 import { MiniHeader } from "@/components/applets/Header";
-import { Section, Sep } from "@/components/basics";
+import { C, I, Section, Sep, T } from "@/components/basics";
 import { SettingsContext } from "@/components/Contexts";
-import { Title, OptionsSetting } from "@/components/settings";
+import { Title, OptionsSetting, BoolSetting } from "@/components/settings";
+import { useColors } from "@/constants/colors";
 import { move } from "@/src/general/funcs";
-import { TTTLevelOptions } from "@/src/general/interfaces";
+import { TTTLevelOptions, TTTPlayers } from "@/src/general/interfaces";
 import { setStored } from "@/src/general/storage";
 import { useContext } from "react";
 import { ScrollView } from "react-native";
 
 
 export default function TicTacToeSettings() {
+  const colors = useColors();
   const { tictactoe } = useContext(SettingsContext).settings;
   const updateSettings = useContext(SettingsContext).updateSettings;
 
@@ -18,12 +20,11 @@ export default function TicTacToeSettings() {
       <MiniHeader title='OPTIONS' subtitle='TIC-TAC-TOE'/>
       <ScrollView style={{ flex: 1 }}>
         <Title title="Game" />
-
         <Sep noThickness />
 
         <OptionsSetting
           title="Difficulty"
-          description={"Decide aggressive or smart the CPU is."}
+          description={"Decide how aggressive or smart the CPU is."}
           index={TTTLevelOptions.indexOf(tictactoe.level)}
           options={TTTLevelOptions}
           onSubmit={(forward:boolean = true) => {
@@ -34,6 +35,30 @@ export default function TicTacToeSettings() {
           size="medium"
         />
 
+        <BoolSetting
+          title="Play vs CPU"
+          description={<>Play solo against the CPU <I>(lonely people mode)</I>.</>}
+          current={tictactoe.vs_cpu}
+          onSubmit={() => {
+            setStored('tictactoe', {...tictactoe, vs_cpu: !tictactoe.vs_cpu });
+            updateSettings();
+          }}
+          size="medium"
+        />
+
+        <OptionsSetting
+          title="CPU Plays as"
+          description={<>Decides whether the CPU play as <C.RED>X</C.RED> or <C.GREEN>O</C.GREEN></>}
+          index={TTTPlayers.indexOf(tictactoe.cpu_as)}
+          onSubmit={(forward:boolean = true) => {
+            setStored('tictactoe', {...tictactoe, cpu_as: move(forward)(TTTPlayers, tictactoe.cpu_as)});
+            updateSettings();
+          }}
+          icon={tictactoe.cpu_as === 1 ? "close-sharp" : "ellipse-outline"}
+          color={tictactoe.cpu_as === 1 ? colors.red : colors.green}
+          size="small"
+          disabled={!tictactoe.vs_cpu}
+        />
       </ScrollView>
     </Section>
   )
